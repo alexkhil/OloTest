b515333e851025b6b1159d9ff5fd9aa3c77f0681
Alex Khil <alexkhil@users.noreply.github.com>
2018-04-19 00:31:26 +0300

    Initial commit

---
+++ src/OloTest.Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OloTest.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test/OloTest.Task2.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace OloTest.Task2.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
